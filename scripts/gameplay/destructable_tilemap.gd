5e2e7baed5f8f347e7df7635f09de17c8fc5f96d
x32dev <120539187+FlexTechDev@users.noreply.github.com>
2024-03-24 10:44:23 -0500

    terrain destruction

--- scripts/gameplay/destructable_tilemap.gd
+++ scripts/gameplay/destructable_tilemap.gd
@@ -6,17 +6,17 @@ class_name DestructableTileMap;
 
 func destroy_tiles_at_point(point: Vector2, range: float) -> void:
 	var hit_cell_location: Vector2i = local_to_map(point);
+	
+	var deleted_cells: Array[Vector2i];
+	
 	for tile in get_used_cells(0):
 		if(Vector2(hit_cell_location).distance_to(Vector2(tile)) < range):
-			erase_cell(0, tile);
-			
-			var neighboring_cells: Array[Vector2i] = get_surrounding_cells(tile);
-			
-			for neighbor in neighboring_cells:
-				if(get_cell_tile_data(0, neighbor) != null):
-					set_cell(0, neighbor, 0, Vector2i(0,0));
+			deleted_cells.append(tile);
 			
 			var particle_instance: GPUParticles2D = destruction_particles.instantiate();
 			add_child(particle_instance);
 			particle_instance.position = map_to_local(tile);
 			particle_instance.emitting = true;
+	
+	BetterTerrain.set_cells(self, 0, deleted_cells, -1);
+	BetterTerrain.update_terrain_cells(self, 0, deleted_cells, true);
